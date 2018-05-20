0e963109b8ab1b09abfcc33d22e7c77741b3d914
Felipe Milani <felmilani@gmail.com>
2018-05-20 16:08:06 -0300

    Create admin package

---
+++ packages/admin/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from 'shared/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
